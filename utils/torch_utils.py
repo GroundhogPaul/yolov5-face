ee4a5fbacd0bf9b0a1aa8389d4840bd4fcb325bf
GroundhogPaul <2265546016@qq.com>
2025-05-15 17:00:34 +0800

    print GFlops

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -219,6 +219,7 @@ def model_info(model, verbose=False, img_size=640):
         fs = ''
 
     logger.info(f"Model Summary: {len(list(model.modules()))} layers, {n_p} parameters, {n_g} gradients{fs}")
+    print(f"Model Summary: {len(list(model.modules()))} layers, {n_p} parameters, {n_g} gradients{fs}")
 
 
 def load_classifier(name='resnet101', n=2):
